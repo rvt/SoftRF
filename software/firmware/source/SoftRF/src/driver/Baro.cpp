d1afe6c56635b47a40dfcc71eeedb97a3c0f7b6c
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-03-03 13:34:38 +0100

    disable BMP280 for Stratux build
    for some strange reason, a BMP280 module prevents some of the u-blox configuration changes

--- software/firmware/source/SoftRF/src/driver/Baro.cpp
+++ software/firmware/source/SoftRF/src/driver/Baro.cpp
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+#define EXCLUDE_BMP280 /* required for Stratux build */
+
 #include "../system/SoC.h"
 
 #include "Baro.h"

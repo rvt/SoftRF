9ba9f63e78ef1fb92492038fa09c3cfacfddc5a3
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-03-15 14:40:17 +0100

    Update Baro.cpp
    no longer required

--- software/firmware/source/SoftRF/src/driver/Baro.cpp
+++ software/firmware/source/SoftRF/src/driver/Baro.cpp
@@ -16,9 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-//#define EXCLUDE_BMP280 /* required for Stratux build */
-
 #include "../system/SoC.h"
 
 #include "Baro.h"

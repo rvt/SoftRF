70e22bc171087a60f2006a4f5e582de02d4a13a2
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-06-19 17:05:58 +0200

    move #define STRATUX to SoftRF.h

--- software/firmware/source/SoftRF/SoftRF.h
+++ software/firmware/source/SoftRF/SoftRF.h
@@ -31,6 +31,8 @@
 #include <raspi/raspi.h>
 #endif /* RASPBERRY_PI */
 
+#define STRATUX /* enable for Stratux build */
+
 #define SOFTRF_IDENT            "SoftRF"
 #define SOFTRF_FIRMWARE_VERSION "1.1"
 #define SOFTRF_USB_FW_VERSION   0x0101
--- software/firmware/source/SoftRF/src/driver/GNSS.cpp
+++ software/firmware/source/SoftRF/src/driver/GNSS.cpp
@@ -16,8 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define STRATUX /* enable for Stratux build */
-
 #if defined(ARDUINO)
 #include <Arduino.h>
 #endif
